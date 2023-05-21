c12a4e479cc7887eb3989dfc83784a8e56c42d0e
medeluxe <techdeluxe00@gmail.com>
2023-05-21 23:53:50 +0000

    added chalk lib for better loggin

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 import { startServer } from "./lib/server.js"
 import { connectDB } from "./lib/db/connect.js"
 import { ExpressApp } from "./lib/setups.js"
+import { loggerMiddleware } from "./lib/middleware.js"
 
 import dotenv from "dotenv"
+import { logError, logSuccess } from "./lib/logger.js"
 
 dotenv.config()
 
+console.log()
 connectDB()
     .then(async (data) => {
-        console.log("db connection secured")
-
         const PORT = process.env.PORT || 9000
 
         const app = ExpressApp()
 
+        app.use(loggerMiddleware)
+
         app.listen(PORT, function () {
-            console.log(`SMTP-Server up and running on port [${PORT}]`)
+            console.log()
+            logSuccess(`DATABASE connection secured 👍👍👍`)
+            logSuccess(`Server up and running on port [${PORT}]`)
             startServer()
         })
     })
     .catch((error) => {
-        console.log(error)
+        logError(error?.message || error)
     })
