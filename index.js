38f612a66fa0f155ab8de59529a787949188dc99
medeluxe <techdeluxe00@gmail.com>
2023-05-21 22:49:40 +0000

    added mongoDB database 4x

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 import { startServer } from "./lib/server.js"
+import { connectDB } from "./lib/db/connect.js"
+import { ExpressApp } from "./lib/setups.js"
 
-startServer()
+import dotenv from "dotenv"
+
+dotenv.config()
+
+connectDB()
+    .then(async (data) => {
+        console.log("db connection secured")
+
+        const PORT = process.env.PORT || 9000
+
+        const app = ExpressApp()
+
+        app.listen(PORT, function () {
+            console.log(`SMTP-Server up and running on port [${PORT}]`)
+            startServer()
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
